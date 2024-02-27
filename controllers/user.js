6bfad2b75e67bce041ca75062aa6722fc8de9b51
Shyamal <info@bigsss.com>
2024-02-27 14:12:05 +0530

    message set

--- controllers/user.js
+++ controllers/user.js
@@ -8,7 +8,7 @@ const Ledger = require("../models/ledger");
 exports.signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error(errors.array()[0].message);
+    const error = new Error(errors.array()[0].msg);
     error.statusCode = 422;
     error.data = errors.array();
     throw error;
